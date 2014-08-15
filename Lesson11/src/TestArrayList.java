50bcf433f31c9603f96336207a66157a3e0dfc31
Martijn Verburg <martijnverburg@gmail.com>
2014-08-15 15:10:01 +0100

    Minor code formatting and coding to interfaces + naming

--- Lesson11/src/TestArrayList.java
+++ Lesson11/src/TestArrayList.java
@@ -1,30 +1,27 @@
 import java.util.ArrayList;
+import java.util.List;
 
 public class TestArrayList {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		
-		ArrayList customers = new ArrayList();
-		Customer cust1 = new Customer("David","Lee");
-		customers.add(cust1);
-		Customer cust2 = new Customer("Ringo","Starr");
-		customers.add(cust2);
-		
-		Order ord = new Order();
-		customers.add(ord);
-		
-		int totalElem = customers.size(); // number of elements
-		
-		// This loop will throw ClassCastException 
-		// on the third iteration
-		for (int i=0; i< totalElem;i++){
-		  Customer currentCust = (Customer) customers.get(i);
-		  
-		}
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
 
-	}
+        List customers = new ArrayList();
+        Customer customer1 = new Customer("David", "Lee");
+        customers.add(customer1);
+        Customer customer2 = new Customer("Ringo", "Starr");
+        customers.add(customer2);
+
+        Order order = new Order();
+        customers.add(order);
+
+        // This loop will throw ClassCastException on the third iteration
+        for (int i = 0; i < customers.size(); i++) {
+            Customer currentCustomer = (Customer) customers.get(i);
+        }
+
+    }
 
 }
--- Lesson11/src/TestVariousCollections.java
+++ Lesson11/src/TestVariousCollections.java
@@ -1,35 +1,29 @@
 import java.util.*;
 import java.util.Queue;
 import java.util.LinkedList;
+
 public class TestVariousCollections {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
 
-		
-		HashSet sss = new HashSet();
-		sss.add("Mary");
-		sss.add("Mary");
-			
-		ArrayList aaa = new ArrayList();
-		
-		aaa.add("Mary");
-		aaa.add("Mary");
-		
-		
-		Vector bbb = new Vector();
-		
-		
-		HashMap hm=new HashMap();
-		hm.put("customer", new Customer("John", "Smith")); 
-		hm.put("order", new Order());
+        HashSet sss = new HashSet();
+        sss.add("Mary");
+        sss.add("Mary");
 
+        ArrayList aaa = new ArrayList();
 
-		
-		
-	}
+        aaa.add("Mary");
+        aaa.add("Mary");
+
+        Vector bbb = new Vector();
+
+        Map map = new HashMap();
+        map.put("customer", new Customer("John", "Smith"));
+        map.put("order", new Order());
+
+    }
 
 }
