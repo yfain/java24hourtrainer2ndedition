9675601c6064f92426b59f46afe84d6ffdf63840
Martijn Verburg <martijnverburg@gmail.com>
2014-10-03 13:58:01 -0700

    suggested changes for CH13

--- Lesson13/src/ActionListenerWithLambda.java
+++ Lesson13/src/ActionListenerWithLambda.java
@@ -1,47 +1,47 @@
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-	import java.awt.*;
-	import java.awt.event.ActionEvent;
-	import java.awt.event.ActionListener;
-
-	import javax.swing.*;
+import javax.swing.*;
 
 public class ActionListenerWithLambda {
 
-	        public static void main(String[] args) {
-	            JButton oldButton = new JButton("I'm an Old Java Button");
-	            JButton newButton = new JButton("I'm brand new Java Button");
-	            JTextArea myConsole = new JTextArea("Click a button");
+	public static void main(String[] args) {
 
-	            //Action Listener with anonymous inner class
-	            oldButton.addActionListener(new ActionListener() {
-	                public void actionPerformed(ActionEvent ae) {
-	                    myConsole.setText("Processing Click with Anonymous Inner Class");
-	                }
-	            });
+		JButton oldButton = new JButton("I'm an Old Java Button");
+		JButton newButton = new JButton("I'm brand new Java Button");
+		JTextArea myConsole = new JTextArea("Click a button");
 
-	            // Action Listener has one callback method actionPerformed()with lambda expression
-	            // Implement it using lambdas
-	            newButton.addActionListener(e -> {
-	                myConsole.setText("Processing Click with Lambda");
-	            });
+		// Action Listener with anonymous inner class
+		oldButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent actionEvent) {
+				myConsole
+						.setText("Processing Click with Anonymous Inner Class");
+			}
+		});
 
-         
-	            JFrame frame = new JFrame("Lambdas in Swing");
+		// Action Listener has one callback method actionPerformed()with lambda
+		// expression
+		// Implement it using lambdas
+		newButton.addActionListener(actionEvent -> {
+			myConsole.setText("Processing Click with Lambda");
+		});
 
-	            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	            frame.add(oldButton, BorderLayout.NORTH);
-	            frame.add(newButton, BorderLayout.CENTER);
-	            frame.add(myConsole, BorderLayout.SOUTH);
+		JFrame frame = new JFrame("Lambdas in Swing");
 
-	            frame.setSize(300,400);
-	            frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.add(oldButton, BorderLayout.NORTH);
+		frame.add(newButton, BorderLayout.CENTER);
+		frame.add(myConsole, BorderLayout.SOUTH);
 
+		frame.setSize(300, 400);
+		frame.setVisible(true);
 
-	            new Thread(new Runnable() {
-	                public void run() {
-	                    System.out.println("Do something in this thread ");
-	                }
-	            });
+		new Thread(new Runnable() {
+			public void run() {
+				System.out.println("Do something in this thread ");
+			}
+		});
 
-	        }
 	}
+}
--- Lesson14/src/TestFileInputStream.java
+++ Lesson14/src/TestFileInputStream.java
@@ -1,22 +1,22 @@
 import java.io.FileInputStream;
 import java.io.IOException;
+
 public class TestFileInputStream {
 
 	public static void main(String[] args) {
 
-		try (FileInputStream myFile = new FileInputStream("abc.dat")){
-		        
-		            boolean eof = false;
+		try (FileInputStream myFile = new FileInputStream("abc.dat")) {
 
-		            while (!eof) {
-		                int byteValue = myFile.read();
-		                System.out.print(byteValue + " ");
-		                if (byteValue  == -1)
-		                    eof = true;
-		           }
-		  } catch (IOException e) {
-		           System.out.println("Could not read file: " + 
-		                                           e.toString());
-		  } 
+			boolean eof = false;
+
+			while (!eof) {
+				int byteValue = myFile.read();
+				System.out.print(byteValue + " ");
+				if (byteValue == -1)
+					eof = true;
+			}
+		} catch (IOException e) {
+			System.out.println("Could not read file: " + e.toString());
 		}
 	}
+}
