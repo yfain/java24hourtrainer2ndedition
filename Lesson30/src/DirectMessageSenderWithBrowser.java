e6d28f3a81ca866bfa9f55d20bdb64e246a980ce
Yakov Fain <yakovfain@gmail.com>
2014-10-10 13:14:14 -0400

    added more code samples to ch30 - JMS

--- Lesson30/src/DirectMessageSenderWithBrowser.java
+++ Lesson30/src/DirectMessageSenderWithBrowser.java
@@ -1,11 +1,13 @@
 
+import java.util.Enumeration;
+
 import javax.jms.*;
 
 import com.sun.messaging.ConnectionFactory;
 import com.sun.messaging.ConnectionConfiguration;
 
 
-public class DirectMessageSender{
+public class DirectMessageSenderWithBrowser{
  public static void main(String[] args){
 
    ConnectionFactory factory;
@@ -15,14 +17,21 @@ public class DirectMessageSender{
 	try( JMSContext context = factory.createContext("admin","admin")){
 
 		factory.setProperty(ConnectionConfiguration.imqAddressList, "mq://127.0.0.1:7676,mq://127.0.0.1:7676");
-		      Destination ordersQueue = context.createQueue("TradingOrdersQueue");	  
+		      Queue ordersQueue = context.createQueue("TradingOrdersQueue");	  
 	          JMSProducer producer = context.createProducer();
 	      
 	          // Send msg to buy 200 shares of IBM at market price	      
-	          producer.send(ordersQueue,"IBM 200 Mkt");
+	          producer.send(ordersQueue,"IBM 300 Mkt");
 	          
-	          System.out.println("Placed an order to purchase 200 shares of IBM to TradingOrdersQueue");
+	          System.out.println("Placed an order to purchase 300 shares of IBM to TradingOrdersQueue");
 	          
+	          QueueBrowser queueBrowser = context.createBrowser(ordersQueue);
+	          Enumeration enumeration = queueBrowser.getEnumeration();
+	          while (enumeration.hasMoreElements()) {
+	              TextMessage o = (TextMessage) enumeration.nextElement();
+	             System.out.println("Browser sees  " + o.getText());
+
+	          }
 	          
 	          
 	          
--- Lesson30/src/DirectObjectMessageSender.java
+++ Lesson30/src/DirectObjectMessageSender.java
@@ -5,7 +5,7 @@ import com.sun.messaging.ConnectionFactory;
 import com.sun.messaging.ConnectionConfiguration;
 
 
-public class DirectMessageSender{
+public class DirectObjectMessageSender{
  public static void main(String[] args){
 
    ConnectionFactory factory;
@@ -17,15 +17,15 @@ public class DirectMessageSender{
 		factory.setProperty(ConnectionConfiguration.imqAddressList, "mq://127.0.0.1:7676,mq://127.0.0.1:7676");
 		      Destination ordersQueue = context.createQueue("TradingOrdersQueue");	  
 	          JMSProducer producer = context.createProducer();
+	          
+	          Order order = new Order(1, "AAPL", 200, 100.00f);
+	          ObjectMessage objMsg = context.createObjectMessage(order);
+	          
 	      
-	          // Send msg to buy 200 shares of IBM at market price	      
-	          producer.send(ordersQueue,"IBM 200 Mkt");
+	          // Send msg to buy 200 shares of AAPL at market price	      
+	          producer.send(ordersQueue,objMsg);
 	          
-	          System.out.println("Placed an order to purchase 200 shares of IBM to TradingOrdersQueue");
-	          
-	          
-	          
-	          
+	          System.out.println("Placed an order to purchase 200 shares of AAPL to TradingOrdersQueue");
 	 } catch (JMSException e){
 	           System.out.println("Error: " + e.getMessage());
 	 } 
