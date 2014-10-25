d10c0a3d879655be6bbdb88e294d036eaa5e291a
Yakov Fain <yakovfain@gmail.com>
2014-10-25 11:54:29 -0400

    minor change in TestHashtable lesson 11

--- Lesson11/src/TestHashtable.java
+++ Lesson11/src/TestHashtable.java
@@ -4,14 +4,14 @@ public class TestHashtable {
 	
 	public static void main(String[] args){
 		
-		Customer cust = new Customer("David", "Lee");
-		Order ord = new Order();
-		Portfolio port = new Portfolio();
+		Customer customer = new Customer("David", "Lee");
+		Order order = new Order();
+		Portfolio portfolio = new Portfolio();
 		
 		Hashtable data = new Hashtable();
-		data.put("Customer", cust);
-		data.put("Order",ord);
-		data.put("Portfolio", port);
+		data.put("Customer", customer);
+		data.put("Order",order);
+		data.put("Portfolio", portfolio);
 		
 		// retrieving the instance of Order by the key "Order"
 		Order myOrder = (Order) data.get("Order");
